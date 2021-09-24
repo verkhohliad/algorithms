4da61e60e2698e4cdbc96545f84c966b6020a22d
Verkhohliad <ihor.verkhohliad@gmail.com>
2021-09-24 11:38:29 +0300

    add recusrion version for fisrtBadVersion problem

--- problems/first-bad-version.js
+++ problems/first-bad-version.js
@@ -20,3 +20,29 @@ function firstBadVersion(isBad) {
     return left > version ? -1 : left
   }
 }
+
+// recursion
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+ var solution = function(isBadVersion) {
+  let left = 0
+  let middle
+  
+  var checkVersion = function(n) {
+      middle = Math.floor((left + n) / 2)
+      
+      if (isBadVersion(middle)) {
+          if (!isBadVersion(middle-1)) return middle
+          
+          n = middle - 1
+      } else {
+          left = middle + 1
+      }
+      
+      return checkVersion(n)
+  };
+  
+  return checkVersion
+};
